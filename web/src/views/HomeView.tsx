3f3e712eede9f33eb7e59ef3c7ae5ffe68552f40
xilnick <dev@aslang.com>
2026-09-07 14:43:01 +0300

    feat(web): migrate KeyCapabilities component to pure AgentScript with full visual fidelity

--- web/src/views/HomeView.tsx
+++ web/src/views/HomeView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Hero } from '../components/Hero';
 import { Ecosystem } from '../components/Ecosystem';
-import KeyCapabilities from '../../asl-src/components/KeyCapabilities.asl';
+import { KeyCapabilities } from '../components/KeyCapabilities';
 import TheAgentWay from '../../asl-src/components/TheAgentWay.asl';
 import { InBrowserAgent } from '../components/InBrowserAgent';
 import { AgentWireProtocol } from '../components/AgentWireProtocol';
